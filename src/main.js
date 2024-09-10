43ee2885f36cd82f6a3aeb997673f6b32f34571e
Sinclair <sinclairolajuwon@gmail.com>
2024-09-10 05:11:16 -0500

    Almost Done

---
+++ src/main.js
@@ -0,0 +1,5 @@
+const email = document.querySelector('input[type="email"]');
+const password = document.getElementById("password");
+const submit = document.getElementById("submit");
+const submit2 = document.getElementById("submit2");
+
