4fa4c68b0eb6aeccc53fa4b33cc6ee6aac746f94
Cyprian Ogbang <119867014+Cyogbang@users.noreply.github.com>
2024-09-13 14:54:39 +0100

    Staging (#7)
    * starting
    
    * signup ui done
    
    * Almost Done
    
    * Done with the rendering
    
    * Done with the rendering
    
    * Created the and styled the chat gpt style
    
    * Chatspace markups
    
    * style sheet added
    
    * images and positioning added from top to chat area
    
    * Nav Bar converted to off-canvas
    
    * More styling fixed
    
    * dropdown styling done
    
    * textarea fixed
    
    * Added icons for the sidebar
    
    * deleted unwated sections
    
    ---------
    
    Co-authored-by: Sinclair <sinclairolajuwon@gmail.com>
    Co-authored-by: iamonuwacj <onuwachinedu66@gmail.com>
    Co-authored-by: Opeyemi Oduyemi <119660096+oduyemi@users.noreply.github.com>

---
+++ src/main.js
@@ -0,0 +1,67 @@
+const signupForm = document.getElementById('signupform');
+const emailInput = document.getElementById('emailInput');
+const passwordInput = document.getElementById('passwordInput');
+const account = document.getElementById('account');
+const extra = document.getElementById('extra');
+const extra1 = document.getElementById('extra1');
+const successMessage = document.getElementById('success');
+const submitButton = document.getElementById('submitbutton');
+const login = document.getElementById('login');
+const signup = document.getElementById('signup');
+const sign = document.getElementById("sign");
+
+
+login.addEventListener("click", () => {
+    account.textContent = "Login to your account";
+    extra.classList.add("hidden");
+    extra1.classList.remove("hidden");
+    submitButton.textContent = "Login";
+});
+
+
+signup.addEventListener("click", () => {
+    account.textContent = "Create an account";
+    extra.classList.remove("hidden");
+    extra1.classList.add("hidden");
+    submitButton.textContent = "Submit";
+});
+
+
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+   
+    const email = emailInput.value;
+    const password = passwordInput.value;
+
+    let users = JSON.parse(localStorage.getItem('users')) || [];
+
+
+    if (submitButton.textContent === "Submit") {
+      
+        const userExists = users.find(user => user.email === email);
+
+        if (userExists) {
+            alert("User already exists! Please login.");
+            return;
+        }
+
+
+        users.push({ email, password });
+        localStorage.setItem('users', JSON.stringify(users));
+
+        alert("Sign up successful! You can now log in.");
+        signupForm.reset();
+    } else {
+        
+        const authenticatedUser = users.find(user => user.email === email && user.password === password);
+
+        if (authenticatedUser) {
+            sign.classList.add("hidden")
+            successMessage.classList.remove('hidden');
+        } else {
+            alert("Invalid email or password! Please try again.");
+        }
+    }
+});
+
