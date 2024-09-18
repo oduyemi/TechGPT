41d99902433e20c4a357aadb72d0f476d31b5d28
Yemi Oduyemi <yhermii@gmail.com>
2024-09-18 13:19:51 +0100

    Amended

--- src/main.js
+++ src/main.js
@@ -1,41 +1,38 @@
 const signupForm = document.getElementById('signupform');
 const emailInput = document.getElementById('emailInput');
 const passwordInput = document.getElementById('passwordInput');
-const confirmPasswordInput = document.getElementById('confirmPasswordInput');
 const account = document.getElementById('account');
 const extra = document.getElementById('extra');
+const extra1 = document.getElementById('extra1');
 const successMessage = document.getElementById('success');
 const submitButton = document.getElementById('submitbutton');
 const login = document.getElementById('login');
 const signup = document.getElementById('signup');
+const sign = document.getElementById("sign");
 
 login.addEventListener("click", () => {
     account.textContent = "Login to your account";
-    extra.classList.add("hidden");
-    submitButton.textContent = "Login";
+    extra.classList.add("hidden");   
+    extra1.classList.remove("hidden"); 
+    submitButton.textContent = "Login"; 
 });
 
 signup.addEventListener("click", () => {
     account.textContent = "Create an account";
-    extra.classList.remove("hidden");
-    submitButton.textContent = "Submit";
+    extra.classList.remove("hidden");   
+    extra1.classList.add("hidden");  
+    submitButton.textContent = "Sign Up";  
 });
 
 signupForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+    e.preventDefault();  
 
     const email = emailInput.value;
     const password = passwordInput.value;
-    const confirmPassword = confirmPasswordInput ? confirmPasswordInput.value : '';
 
     let users = JSON.parse(localStorage.getItem('users')) || [];
 
-    if (submitButton.textContent === "Submit") {
-        if (password !== confirmPassword) {
-            alert("Passwords do not match!");
-            return;
-        }
-
+    if (submitButton.textContent === "Sign Up") {
         const userExists = users.find(user => user.email === email);
 
         if (userExists) {
@@ -47,12 +44,13 @@ signupForm.addEventListener('submit', (e) => {
         localStorage.setItem('users', JSON.stringify(users));
 
         alert("Sign up successful! You can now log in.");
-        window.location.href = 'chat.html'; 
+        signupForm.reset();  
     } else {
         const authenticatedUser = users.find(user => user.email === email && user.password === password);
 
         if (authenticatedUser) {
-            window.location.href = 'chat.html'; 
+            sign.classList.add("hidden");
+            successMessage.classList.remove('hidden');
         } else {
             alert("Invalid email or password! Please try again.");
         }
